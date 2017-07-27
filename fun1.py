070c5c14630ffb4cc7951d0e63c7e73526086c67
noamb19-meet <noamb19@meet.mit.edu>
2017-07-27 14:03:46 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,23 @@
+##c=0
+##for number in range (1,100+1):
+##    print(number)
+##    c=c+number
+##print (c)
+
+
+def add_numbers(start,end):
+    c=0
+    for number in range(333,778):
+        print(number)
+        c=c+number
+    return c
+
+
+test1=add_numbers(1,2)
+print(test1)
+test2=add_numbers(1,100)
+print(test2)
+test3=add_numbers(1000,5000)
+print(test3)
+
+    
