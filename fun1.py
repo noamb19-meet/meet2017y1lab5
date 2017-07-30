70a8fade69505478cd38d51c9386fb2e5c6b5ead
noamb19-meet <noamb19@meet.mit.edu>
2017-07-30 13:55:08 +0300

    Stopping here

--- fun1.py
+++ fun1.py
@@ -7,8 +7,8 @@
 
 def add_numbers(start,end):
     c=0
-    for number in range(333,778):
-        print(number)
+    for number in range(start, end + 1):
+##        print(number)
         c=c+number
     return c
 
---
+++ fun2.py
@@ -0,0 +1,13 @@
+def draw_1d(n,char):
+    print(n * char)
+
+
+def draw_2d(n, m, char):
+    for num in range(n):
+        draw_1d(m, char)
+
+def special_draw_2d(n,m,border,fill):
+    
+    
+   
+    
